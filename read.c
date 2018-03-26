9afa4b99467f9443ea78189a36fc79a73814729f
atikhono <tihonovandrey11@gmail.com>
2018-03-26 18:40:03 +0300

    work started

--- read.c
+++ read.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   read.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/10 15:56:24 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/26 18:39:16 by atikhono         ###   ########.fr       */
+/*   Created: 2018/03/26 17:43:30 by atikhono          #+#    #+#             */
+/*   Updated: 2018/03/26 17:43:32 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
 
-int		main(int ac, char **av)
-{
-	void	*mlx = mlx_init();
-	void	*win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
-	mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
-	mlx_loop(mlx);
-
-	//call parser
-	//initialise window
-	//call hookers
-	//call loop to loop
-
-	return (0);
-}
