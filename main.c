c0b1c9e56d0765a146d3057d32dfe2eafcbc8f4d
atikhono <tihonovandrey11@gmail.com>
2018-03-29 17:54:37 +0300

    small edit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 15:56:24 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/29 17:11:13 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/03/29 17:48:33 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		main(int ac, char **av)
 {
-	if (!parce())
-		return (1);
+//	if (!parce())
+//		return (1);
 	void	*mlx = mlx_init();
 	void	*win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
 	mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
