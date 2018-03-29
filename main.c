10a52a32403f3d1f7e1b42af466347a77b8a9cf3
atikhono <tihonovandrey11@gmail.com>
2018-03-29 17:12:33 +0300

    rearrenge calls in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 15:56:24 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/26 18:39:16 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/03/29 17:11:13 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 int		main(int ac, char **av)
 {
+	if (!parce())
+		return (1);
 	void	*mlx = mlx_init();
 	void	*win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
 	mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
-	mlx_loop(mlx);
-
-	//call parser
-	//initialise window
 	//call hookers
-	//call loop to loop
+	mlx_loop(mlx);
 
 	return (0);
 }
