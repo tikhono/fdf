9b876b32109e7176464de3b5342cc6024ff4f66d
atikhono <tihonovandrey11@gmail.com>
2018-03-29 20:03:17 +0300

    debug mlx init

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 15:56:24 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/29 19:48:21 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/03/29 20:01:59 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
+#include <unistd.h>
+#include "libft/libft.h"
 
 int		main(int ac, char **av)
 {
@@ -19,22 +21,20 @@ int		main(int ac, char **av)
 
 	if (ac < 2)
 		return (1);
-	*mlx = mlx_init();
 	while (ac > 1)
-	{
 		if (fork() != 0)
 			--ac;
 		else
 			break ;
-		if (parse(av[ac]))
-		{
-		*win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
-		mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
-	//call hookers
-		mlx_loop(mlx);
-		}
-		else
-			ft_putstr("Invalid");
+	if (1)//parse(av[ac])
+	{
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
+	mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
+//call hookers
+	mlx_loop(mlx);
 	}
+	else
+		ft_putstr("Invalid");
 	return (0);
 }
