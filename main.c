e2b71ae089190c0ffee45d28523975f9b318e68f
atikhono <tihonovandrey11@gmail.com>
2018-03-29 19:46:21 +0300

    going to use fork

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 15:56:24 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/29 17:48:33 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/03/29 19:45:43 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,24 @@
 
 int		main(int ac, char **av)
 {
-//	if (!parce())
-//		return (1);
-	void	*mlx = mlx_init();
-	void	*win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
-	mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
-	//call hookers
-	mlx_loop(mlx);
+	void	*mlx;
+	void	*win;
 
+	if (ac < 2)
+		return (1);
+	*mlx = mlx_init();
+	while (ac > 1)
+	{
+		if (parse(av[ac]))
+		{
+		*win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
+		mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
+	//call hookers
+		mlx_loop(mlx);
+		}
+		else
+			ft_putstr("Invalid");
+		--ac;
+	}
 	return (0);
 }
