0a9505593965fbd986d431b9afa97608c4a42b85
atikhono <tihonovandrey11@gmail.com>
2018-04-01 12:42:58 +0300

    fix number of windows

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 15:56:24 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/29 20:01:59 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/04/01 12:41:43 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,20 @@ int		main(int ac, char **av)
 
 	if (ac < 2)
 		return (1);
-	while (ac > 1)
+	while (ac > 2)
 		if (fork() != 0)
 			--ac;
 		else
 			break ;
 	if (1)//parse(av[ac])
 	{
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 1200, 600, "Ya okoshko");
-	mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);	
-//call hookers
-	mlx_loop(mlx);
+		mlx = mlx_init();
+		win = mlx_new_window(mlx, 1200, 600, ft_itoa(ac - 1));
+		mlx_pixel_put(mlx, win, 100, 100, 0xFF0000);
+		//call_hookers;
+		mlx_loop(mlx);
 	}
 	else
-		ft_putstr("Invalid");
+		ft_putstr("Invalid map");
 	return (0);
 }
