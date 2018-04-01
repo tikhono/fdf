7de7d7f6191f915735a31a8636829e271e8a58c9
atikhono <tihonovandrey11@gmail.com>
2018-04-01 15:56:43 +0300

    validation of empty file and unreadble

--- parser.c
+++ parser.c
@@ -6,31 +6,44 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 17:57:48 by atikhono          #+#    #+#             */
-/*   Updated: 2018/04/01 13:12:44 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/04/01 15:53:27 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <unistd.h>
 #include "libft/libft.h"
-#include <printf.h>
+
+int		number_of_rows(int fd)
+{
+	int		end;
+	int		res;
+	char	*line;
+
+	res = 0;
+	end = read(256, line);
+	if (end != -1)
+		end 
+	while ()
+
+	return (res);
+}
+
 int		parse(char *file)
 {
 	int		fd;
+	int		rows;
 	char	*line;
 
-	
-	printf("\n%s\n", file);
-
 	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	rows = number_of_rows(fd);
+	if (rows < 0)
+		return (0);
 	get_next_line(fd, &line);
-	printf("\n%s\n", line);
-
-
-
-
 
 
 
 
 	return (1);
-}
