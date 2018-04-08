02cdf599a2718c6fcb0efcef292eb21acb90cf8c
atikhono <tihonovandrey11@gmail.com>
2018-04-08 16:31:07 +0300

    show color in main.c

--- main.c
+++ main.c
@@ -66,7 +66,7 @@ int		main(int ac, char **av)
 			j = 0;
 			while (arr[i][j] != NULL)
 			{
-				printf("%i  ", arr[i][j]->oz);
+				printf("%i\t%i\t", arr[i][j]->oz, arr[i][j]->color);
 				++j;
 			}
 			printf("\n");
