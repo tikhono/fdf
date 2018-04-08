ed77690cf79097cddb739456ff09920dfb866ea2
atikhono <tihonovandrey11@gmail.com>
2018-04-08 20:19:55 +0300

    add file for atoi_base and fix clion Cmake

--- main.c
+++ main.c
@@ -36,18 +36,30 @@ int		main(int ac, char **av)
 	arr = parse(av[ac]);
 	if (arr)
 	{
-	////mlx = mlx_init();
-	////win = mlx_new_window(mlx, 1200, 600, ft_itoa(ac));
-	//	mlx_pixel_put(mlx, win, 100, 100, 0x00FF0000); //bright
-	//	mlx_pixel_put(mlx, win, 90, 100, 0x1AFF0000);
-	//	mlx_pixel_put(mlx, win, 80, 100, 0x33FF0000);
-	//	mlx_pixel_put(mlx, win, 70, 100, 0x4DFF0000);
-	//	mlx_pixel_put(mlx, win, 60, 100, 0x66FF0000);
-	//	mlx_pixel_put(mlx, win, 50, 100, 0x80FF0000);
-	//	mlx_pixel_put(mlx, win, 40, 100, 0x99FF0000);
-	//	mlx_pixel_put(mlx, win, 30, 100, 0xA6FF0000);
-	//	mlx_pixel_put(mlx, win, 20, 100, 0xBFFF0000);
-	//	mlx_pixel_put(mlx, win, 10, 100, 0xD9FF0000); //dark
+		mlx = mlx_init();
+		win = mlx_new_window(mlx, 1200, 600, ft_itoa(ac));
+		mlx_pixel_put(mlx, win, 100, 100, 0x00FF0000); //bright
+		mlx_pixel_put(mlx, win, 90, 100, 0x1AFF0000);
+		mlx_pixel_put(mlx, win, 80, 100, 0x33FF0000);
+		mlx_pixel_put(mlx, win, 70, 100, 0x4DFF0000);
+		mlx_pixel_put(mlx, win, 60, 100, 0x66FF0000);
+		mlx_pixel_put(mlx, win, 50, 100, 0x80FF0000);
+		mlx_pixel_put(mlx, win, 40, 100, 0x99FF0000);
+		mlx_pixel_put(mlx, win, 30, 100, 0xA6FF0000);
+		mlx_pixel_put(mlx, win, 20, 100, 0xBFFF0000);
+		mlx_pixel_put(mlx, win, 10, 100, 0xD9FF0000); //dark
+		mlx_pixel_put(mlx, win, 70, 110, 0x4DFF0000);
+		mlx_pixel_put(mlx, win, 70, 110, 0x4DFF0000);
+		mlx_pixel_put(mlx, win, 60, 110, 0x66FF0000);
+		mlx_pixel_put(mlx, win, 60, 110, 0x66FF0000);
+		mlx_pixel_put(mlx, win, 50, 110, 0xD9FF0000); //dark
+		mlx_pixel_put(mlx, win, 50, 110, 0xD9FF0000); //dark
+		mlx_pixel_put(mlx, win, 50, 110, 0xD9FF0000); //dark
+		mlx_pixel_put(mlx, win, 50, 110, 0xD9FF0000); //dark
+		mlx_pixel_put(mlx, win, 50, 110, 0x00FF0000); //bright
+		mlx_pixel_put(mlx, win, 50, 110, 0x00FF0000); //bright
+		mlx_pixel_put(mlx, win, 40, 110, 0x80FF0000); //dark
+		mlx_pixel_put(mlx, win, 40, 110, 0x80FF0000); //dark
     //
 	//	draw_line(100, 100, 200, 200, mlx, win);
 	//	draw_line(100, 150, 500, 252, mlx, win);
@@ -72,7 +84,7 @@ int		main(int ac, char **av)
 			printf("\n");
 			++i;
 		}
-	////mlx_loop(mlx);
+		mlx_loop(mlx);
 	}
 	else
 		ft_putstr("Invalid map");
