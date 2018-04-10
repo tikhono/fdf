d5f300e8528f13c35b9cc8f28eb281008afd59c8
atikhono <tihonovandrey11@gmail.com>
2018-04-10 17:45:49 +0300

    make one header

--- main.h
+++ main.h
@@ -1,15 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   line.h                                             :+:      :+:    :+:   */
+/*   main.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/04 16:34:43 by atikhono          #+#    #+#             */
-/*   Updated: 2018/04/05 18:53:29 by atikhono         ###   ########.fr       */
+/*   Created: 2018/04/10 17:41:57 by atikhono          #+#    #+#             */
+/*   Updated: 2018/04/10 17:45:22 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define FACTOR 3.9015625
 
+#ifndef PARSER_H
+# define PARSER_H
+# define FACTOR 3.9015625
+
+typedef struct		s_pix
+{
+	int				x;
+	int				y;	
+	int				z;
+	int				oz;
+	int				color;
+}					t_pix;
+
+t_pix	***parse(char *file);
+int		ipart(double x);
+int		rpart(double x);
+double	fpart(double x);
+double	rfpart(double x);
 void	draw_line(int x0, int y0, int x1, int y1, void *mlx, void *win);
+
+#endif
